2c60f109bb4b66f05ff6d9ef97d2e19fc3f042fa
ZZZHANG-jx <761773882@qq.com>
2025-01-28 15:47:12 +0800

    update shadow_extraction

--- data/preprocess/shadow_extraction.py
+++ data/preprocess/shadow_extraction.py
@@ -11,13 +11,12 @@ from MBD import mask_base_dewarper
 def shadowExtract(cap_im, alb_im):
     im = cap_im
     alb = alb_im
-    _, mask = cv2.threshold(cv2.cvtColor(alb,cv2.COLOR_BGR2GRAY), 1, 255, cv2.THRESH_BINARY)
-
 
     ## Avoid some bad cases
     skip = False
     im_min = np.min(im,axis=-1)
     kernel = np.ones((3,3))
+    _, mask = cv2.threshold(cv2.cvtColor(alb,cv2.COLOR_BGR2GRAY), 1, 255, cv2.THRESH_BINARY)
     mask_erode = cv2.dilate(mask,kernel=kernel)
     mask_erode = cv2.erode(mask_erode,kernel=kernel)
     mask_erode = cv2.erode(mask_erode,iterations=4,kernel=kernel)
@@ -39,20 +38,23 @@ def shadowExtract(cap_im, alb_im):
 
 
 cap_im = cv2.imread('./data/images/2.png')
-mask_im = cv2.imread('./data/images/1.png')[:,:,0]
-mask_im[mask_im>100] = 255
-mask_im[mask_im<100] = 0
 alb_im = cv2.imread('./data/images/3.png')
 
+## get mask by binarizing alb_im
+_, mask = cv2.threshold(cv2.cvtColor(alb,cv2.COLOR_BGR2GRAY), 1, 255, cv2.THRESH_BINARY)
+kernel = np.ones((3,3))
+mask = cv2.dilate(mask,iterations=2,kernel=kernel)
+mask = cv2.erode(mask,iterations=2,kernel=kernel)
+
+## dewarp cap and alb based on the mask by using MBD method
 cap_im, _ = mask_base_dewarper(cap_im, mask_im)
 alb_im, _ = mask_base_dewarper(alb_im, mask_im)
 
 
-shadow_im,skip = shadowExtract(cap_im,alb_im)
-
-## It is recommended to skip this sample if skip is True. Based on our observations, images that meet this condition often introduce noise.
+shadow_im,skip = shadowExtract(cap_im,alb_im) # It is recommended to skip this sample if skip is True. Based on our observations, images that meet this condition often introduce noise.
 
 cv2.imshow('shadow_im',shadow_im)
 cv2.imshow('cap_im',cap_im)
 cv2.imshow('alb_im',alb_im)
+cv2.imshow('mask',mask)
 cv2.waitKey(0)
